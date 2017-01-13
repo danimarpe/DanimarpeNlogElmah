046e5cb6020105395c6795d7cfe7299a65f0e53b
Danimarpe <danimarpe@gmail.com>
2017-01-13 13:16:35 +0100

    Added some examples of all Nlog methods.

--- DanimarpeNlogElmah/DanimarpeNlogElmah/Controllers/HomeController.cs
+++ DanimarpeNlogElmah/DanimarpeNlogElmah/Controllers/HomeController.cs
@@ -8,7 +8,12 @@ namespace DanimarpeNlogElmah.Controllers
         // GET: Home
         public ActionResult Index()
         {
+            Logger.LoggerManager.Instance.Elmah.Debug("Hello");
             Logger.LoggerManager.Instance.Elmah.Error("Oh no, an error ocurred!");
+            Logger.LoggerManager.Instance.Elmah.Fatal("Oh no, a REAL error ocurred!");
+            Logger.LoggerManager.Instance.Elmah.Warn("Meh, shit happens");
+            Logger.LoggerManager.Instance.Elmah.Trace("You are here");
+            Logger.LoggerManager.Instance.Elmah.Info("And this program is ending");
 
             return View();
         }
